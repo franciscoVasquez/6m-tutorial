d511e773c43acf94a22f2a000c9fcc0c718bddf8
la.francisco.javierv <francisco.jaivervazquez@tui.be>
2020-02-19 16:09:04 +0100

    add event subscriber and publisher

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,49 @@
 export class Tui6mTutorialComponent extends HTMLElement {
+
+   
     constructor() {
         super()
         this.shadow = this.attachShadow({ mode: 'open' })
+
+        this.addEventListener("click", ev => {
+            let rect = ev.target.getBoundingClientRect()
+
+            tuiCottonBall.publish(
+                "tutorial-component",
+                this.getAttribute("scope") || "*",
+                "click",
+                { xPos: ev.clientX - rect.left, yPos: ev.clientY - rect.top }
+            )
+            tuiCottonBall.subscribe(
+                "tutorial-component",
+                this.getAttribute("scope") || "*",
+                "background.change",
+                (componentName, scope, eventName, data) => {
+                    this.style.backgroundColor = data.color;
+                }
+            )
+           
+
+        })
+
     }
+
+
+    static get observedAttributes() {
+        return ['level', 'scope']
+    }
+    
 
     connectedCallback() {
         this.shadow.innerHTML = `
           <style>
+            :host([level="warning"]) {
+                border-color: #ff9900
+            }
+
+            :host([level="alert"]) {
+                border-color: #900
+            }
             :host {
                 /* The "all" property resets any CSS inherited from the outer context */
                 all: initial;
@@ -15,8 +52,9 @@ export class Tui6mTutorialComponent extends HTMLElement {
                 background: #daffd5;
                 padding: 20px;
             }
+
           </style>
-          <div>Hello World!</div>
+          <div>Hello World</div>
       `
     }
 }

4d013dfb45e9ef443e573955100fd1e671620fa2
Francisco Vasquez <francisco.javiervazquez@tui.be>
2020-02-20 11:49:41 +0100

    adding files

---
+++ middlelayer/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const server = express()
+const port = process.env.PORT || 80;
+
+server.listen(port, () => console.log(`Server started listening on http://localhost:${port}.`))
+
+server.get('/', (req, res) => {
+    res.send({ value: Math.round(Math.random() * 10000) })
+})
+
+server.get('/health', (req, res) => res.send('OK'))
+
+server.get('/monitor', (req, res) => res.send({ self: "OK" }))
